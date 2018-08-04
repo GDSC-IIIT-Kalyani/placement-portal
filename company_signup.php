8c94946f8df539fcf59371c2ed975c330b9b55fb
Anubhav Patel <anubhavp28@gmail.com>
2018-08-04 18:10:49 +0530

    too many changes

--- company_signup.php
+++ company_signup.php
@@ -1,4 +1,11 @@
 <?php
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
+    require 'PHPMailer\src\Exception.php';
+    require 'PHPMailer\src\PHPMailer.php';
+    require 'PHPMailer\src\SMTP.php';
     
     $data = json_decode($_POST['data']);
 
@@ -39,6 +46,42 @@
     echo "Company Successfully Registered.";
 
     // kind of weird, but thats how PDO connections are closed.
-    $pdo = null
+    $pdo = null;
 
+    $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+    try {
+        //Server settings
+        $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+        $mail->isSMTP();                                      // Set mailer to use SMTP
+        $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+        $mail->SMTPAuth = true;                               // Enable SMTP authentication
+        $mail->Username = 'placement.iiitk@gmail.com';                 // SMTP username
+        $mail->Password = 'iiitkalyani';                           // SMTP password
+        $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+        $mail->Port = 587;                                    // TCP port to connect to
+
+        $mail->SMTPOptions = array(
+            'ssl' => array(
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+                'allow_self_signed' => true
+            )
+        );
+        //Recipients
+        $mail->setFrom('placement.iiitk@gmail.com', 'Placement Cell, IIIT Kalyani');
+        $mail->addAddress('anubhavp28@gmail.com', 'Anubhav Patel');     // Add a recipient
+
+        //Attachments
+
+        //Content
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = 'Here is the subject';
+        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+    }
 ?>
