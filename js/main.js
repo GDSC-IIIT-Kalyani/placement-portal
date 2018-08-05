ecc7dd504dffa2ce7d066305aa43b18e4a391515
Jyotika Yadav <jyotikayadav024@gmail.com>
2018-08-05 19:07:28 +0530

    scrolling in internal links

--- js/main.js
+++ js/main.js
@@ -153,3 +153,18 @@ function companySignUp() {
         },
     });
 }
+
+<!-- Smooth internal link scroll -->
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 700, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
